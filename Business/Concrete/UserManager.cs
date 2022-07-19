97c66bd1d86383d847b0bd886f0bae310b1760e1
AykhanLine <ayxankerimli2001@mail.ru>
2022-07-19 14:28:17 +0400

    Add project files.

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserDal _userDal; //generate contructer ver
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            return _userDal.Get(x => x.Email == email);
+        }
+
+        public void Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {}
+
+       public  DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Role>  Roles { get; set; }
+
+
+    }
+}
